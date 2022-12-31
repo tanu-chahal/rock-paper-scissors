9f7ef110a8744179e15132f0f0123a264e580735
Tanu Chahal <tanuchahal0004@gmail.com>
2022-12-31 21:51:28 +0530

    v1 basic functionality works

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: { colors: {
+      'darkText': "hsl(229, 25%, 31%)",
+      "headerOutline": "hsl(217, 16%, 45%)",
+      "scoreText": "hsl(229, 64%, 46%)"
+  },
+},
+  },
+  plugins: [],
+}
